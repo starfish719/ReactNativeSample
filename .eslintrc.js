31e3feb05e1704721628098ce4ee0cbb92b13042
toda <starfish719@gmail.com>
2020-04-16 00:43:43 +0900

    add lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  "env": {
+    "es6": true,
+    "node": true,
+    "jest": true,
+  },
+  "parser": "babel-eslint",
+  "plugins": [
+    "react",
+    "prettier",
+  ],
+  "parserOptions": {
+    "version": 2018,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true,
+    },
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "prettier/react",
+  ],
+  "rules": {
+    "prettier/prettier": "error",
+  },
+}
