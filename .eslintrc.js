716dcadf5577a273ae478e560ac82e64a28602d4
toda <starfish719@gmail.com>
2020-04-16 01:07:18 +0900

    exec lint to tsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,11 +17,39 @@ module.exports = {
     },
   },
   "extends": [
-    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
-    "prettier/react",
   ],
   "rules": {
+    "no-console": 0,
     "prettier/prettier": "error",
+    "@typescript-eslint/no-var-requires": 0,
+    "@typescript-eslint/indent": 0,
+    "react/jsx-filename-extension": [
+      1,
+      {
+        "extensions": [".ts", ".tsx", ".js", ".jsx"]
+      }
+    ],
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
+      {
+        "multiline": {
+          "delimiter": "none",
+          "requireLast": false
+        },
+        "singleline": {
+          "delimiter": "semi",
+          "requireLast": false
+        }
+      }
+    ],
+    "react/prop-types": 0,
+    "@typescript-eslint/no-use-before-define": 0,
+    "@typescript-eslint/camelcase": 0,
+    "@typescript-eslint/no-unused-vars": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/no-empty-function": 0
   },
 }
